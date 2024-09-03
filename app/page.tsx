d37ea5665d61c723967cb1fc260b69a02806bdf1
says10 <142166942+says10@users.noreply.github.com>
2024-09-03 19:28:26 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,36 @@
 'use client';
 
-import { NextPage } from 'next';
-import { useRouter } from 'next/navigation';
-import { useSession } from 'next-auth/react';
-import Image from 'next/image';
+import React from 'react';
+import _ from 'lodash';
 
-// Define your image paths
-const imagePaths = [
-  '/images/default-blue.png',
-  '/images/default-green.png',
-  '/images/default-red.png',
-  '/images/default-slate.png',
-  // Add more image paths as needed
-];
+import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+import useMovieList from '@/hooks/useMovieList';
+import useFavorite from '@/hooks/useFavorite';
+import InfoModal from '@/components/InfoModal';
+import useInfoModal from '@/hooks/useInfoModal';
 
-// Function to get a random image
-const getRandomImage = () => {
-  const randomIndex = Math.floor(Math.random() * imagePaths.length);
-  return imagePaths[randomIndex];
-};
+const Home = () => {
+  const { data: movies = [], isLoading } = useMovieList();
+  const { data: favorites = [] } = useFavorite();
 
-const Page: NextPage = () => {
-  const router = useRouter();
-  const { data: session } = useSession();
-  const randomImagePath = getRandomImage(); // Get a random image path
+  const { isOpen, closeModal } = useInfoModal();
 
   return (
-    <div className='flex items-center h-screen justify-center'>
-      <div className='flex flex-col'>
-        <h1 className='text-3xl md:text-5xl text-white text-center'>
-          Who is Watching?
-        </h1>
+    <>
+      <InfoModal visible={isOpen} onClose={closeModal} />
+      <Navbar />
+      <Billboard />
+      <div className='pb-40'>
+        <MovieList data={movies} isLoading={isLoading} title='Trending Now' />
 
-        <div className='flex items-center justify-center gap-8 mt-10'>
-          <div onClick={() => router.replace('/')}>
-            <div className='group flex-row w-44 mx-auto'>
-              <div className='relative w-40 h-40 rounded-md items-center justify-center border-2 border-transparent group-hover:cursor-pointer group-hover:border-white overflow-hidden'>
-                <Image
-                  fill
-                  style={{ objectFit: 'contain' }}
-                  src={randomImagePath} // Use the random image path
-                  alt='Profile'
-                />
-              </div>
-              <div className='mt-4 text-neutral-400 text-center group-hover:text-white'>
-                {session?.user?.name}
-              </div>
-            </div>
-          </div>
-        </div>
+        {!_.isEmpty(favorites) && (
+          <MovieList data={favorites} isLoading={isLoading} title='My List' />
+        )}
       </div>
-    </div>
+    </>
   );
 };
 
-export default Page;
+export default Home;
