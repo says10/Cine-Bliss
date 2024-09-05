48a822ea93fdd5fd8d1919c402ef21998ac760e2
says10 <142166942+says10@users.noreply.github.com>
2024-09-05 20:51:18 +0530

    Update route.ts

--- app/api/movie/[movieId]/route.ts
+++ app/api/movie/[movieId]/route.ts
@@ -10,26 +10,34 @@ export async function GET(
   try {
     const { movieId } = params;
     const session = await getServerSession(authOptions);
+
+    // Check for unauthenticated access
     if (!session) {
-      return NextResponse.json('Unauthenticated!!!', { status: 500 });
+      return NextResponse.json('Unauthenticated!', { status: 401 });
     }
 
+    // Validate movieId
     if (typeof movieId !== 'string' || !movieId) {
-      return NextResponse.json('Invalid ID!', { status: 500 });
+      return NextResponse.json('Invalid movie ID!', { status: 400 });
     }
 
+    // Fetch movie from database
     const movie = await prisma.movie.findUnique({
       where: {
         id: movieId,
       },
     });
 
+    // Check if movie exists
     if (!movie) {
-      return NextResponse.json('Invalid ID!', { status: 500 });
+      return NextResponse.json('Movie not found!', { status: 404 });
     }
     
     return NextResponse.json(movie, { status: 200 });
   } catch (error) {
-    return NextResponse.json(error, { status: 400 });
+    // Log error to help with debugging
+    console.error('Error fetching movie:', error);
+
+    return NextResponse.json('Internal server error.', { status: 500 });
   }
 }
